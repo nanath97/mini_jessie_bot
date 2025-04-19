9681636672a85bbb7c7d45c11769555ffbbb05d0
nathan <vinteo.ac@gmail.com>
2025-04-19 14:48:25 -0500

    Ajout de la logique des boutons dans le webhook

--- bott_webhook.py
+++ bott_webhook.py
@@ -18,7 +18,7 @@ WEBHOOK_URL = "https://" + RENDER_WEB_SERVICE_NAME + ".onrender.com" + WEBHOOK_P
 # === Configuration du bot et de FastAPI ===
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
-Bot.set_current(bot)  # Important pour le contexte de aiogram
+Bot.set_current(bot)
 dp = Dispatcher(bot)
 app = FastAPI()
 
@@ -29,15 +29,28 @@ async def on_startup():
     if webhook_info.url != WEBHOOK_URL:
         await bot.set_webhook(url=WEBHOOK_URL)
 
-# === Handler de test /start ===
+# === Commande /start avec les boutons ===
 @dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message):
-    await message.reply("Bienvenue, ton bot fonctionne en mode Webhook !")
+async def send_welcome(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Discuter en tant que VIP", "Voir le contenu du jour", "Juste discuter"]
+    keyboard.add(*buttons)
 
-# === Réponse par défaut ===
-@dp.message_handler()
-async def echo_handler(message: types.Message):
-    await message.reply(f"Tu as dit : {message.text}")
+    await message.answer("Salut mon coeur ! Que veux-tu faire ?", reply_markup=keyboard)
+
+# === Réponse pour chaque bouton ===
+
+@dp.message_handler(lambda message: message.text == "Discuter en tant que VIP")
+async def show_preview(message: types.Message):
+    await message.answer("Nous allons faire connaissance, je t'offrirai des cadeaux surprises et je vais te montrer mon corps sous toutes ses formes! Tu pourras les débloquer après achat. Clique ici : https://t.me/+Kk86-FYp4S05OWQ0")
+
+@dp.message_handler(lambda message: message.text == "Voir le contenu du jour")
+async def offer(message: types.Message):
+    await message.answer("Aujourd’hui, tu peux débloquer 1 vidéo de moi me doigtant comme une coquine dans ma salle de bain, plus 1 figurine digitale de ma miniature pour seulement 39€. Offre valable pendant 1 heure. Clique ici :https://t.me/+8Chmd4e9zVRjZjVk")
+
+@dp.message_handler(lambda message: message.text == "Juste discuter")
+async def chat(message: types.Message):
+    await message.answer("Tu peux m’écrire ici directement. Mais sache que mes contenus exclusifs sont réservés aux abonnés ! Et ce n'est pas sûr que je te réponde de suite...")
 
 # === Route webhook pour Telegram ===
 @app.post(WEBHOOK_PATH)
