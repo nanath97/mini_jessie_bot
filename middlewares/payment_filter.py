9762b8f392473f56932629e8ece9c22bb453705b
nathan <vinteo.ac@gmail.com>
2025-04-28 07:47:20 -0500

    regles admin

--- middlewares/payment_filter.py
+++ middlewares/payment_filter.py
@@ -3,16 +3,14 @@ from aiogram import types
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.dispatcher.handler import CancelHandler
 
-
-ADMIN_ID = 7334072965  # Remets ici ton vrai ID Telegram admin
-
+ADMIN_ID = 7334072965  # Ton ID Telegram admin
 
 BOUTONS_AUTORISES = [
     "🔞Voir la vidéo du jour",
     "✨Discuter en tant que VIP",
     "👀Je suis un voyeur",
-    "✅ Oui je confirme (bannir)",  # <-- ton premier sous bouton
-    "🚀 Non, je veux rejoindre le VIP"  # <-- ton deuxième sous bouton
+    "✅ Oui je confirme (bannir)",
+    "🚀 Non, je veux rejoindre le VIP"
 ]
 
 class PaymentFilterMiddleware(BaseMiddleware):
@@ -29,27 +27,23 @@ class PaymentFilterMiddleware(BaseMiddleware):
         if message.from_user.id == ADMIN_ID:
             if lien_non_autorise(message.text):
                 try:
-                    await message.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-                    await message.bot.send_message(chat_id=message.chat.id, text="🚫 Seuls les liens autorisés sont acceptés.")
+                    await message.delete()
+                    await message.answer("🚫 Seuls les liens autorisés sont acceptés.")
                 except Exception as e:
                     print(f"Erreur suppression lien admin : {e}")
                 raise CancelHandler()
             return  # Sinon laisser passer normal
 
         if message.text and message.text.startswith("/start"):
-            return  # Laisser passer /start normal
+            return  # Laisser passer /start
         
-        # >>> NOUVEL AJOUT TRES IMPORTANT
         if message.text.strip() in BOUTONS_AUTORISES:
-            return
+            return  # Laisser passer les boutons autorisés
         
         if message.from_user.id not in self.authorized_users:
             try:
-                await message.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+                await message.delete()
             except Exception as e:
                 print(f"Erreur suppression message non autorisé : {e}")
-            await message.bot.send_message(
-                message.chat.id,
-                "🚫 Merci de souscrire à un accès VIP ou d’acheter un contenu pour pouvoir discuter."
-            )
+            await message.answer("🚫 Merci de souscrire à un accès VIP ou d’acheter un contenu pour pouvoir discuter.")
             raise CancelHandler()
