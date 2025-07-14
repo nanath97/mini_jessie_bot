632cd05b3d7840b654c584f308559307de69e174
nathan <vinteo.ac@gmail.com>
2025-07-14 10:13:55 -0500

    stripe modification

--- core.py
+++ core.py
@@ -1,15 +1,21 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import os
 from dotenv import load_dotenv
 from middlewares.payment_filter import PaymentFilterMiddleware
 
-
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 bot = Bot(token=BOT_TOKEN)
 bot.set_current(bot)
-dp = Dispatcher(bot)
+
+# ✅ Ajout du MemoryStorage pour le FSM
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
 # ===== AJOUT NOVA PROTECTION PAIEMENT (NE PAS TOUCHER) =====
 authorized_users = set()
+
 # ===== Activation du middleware =====
 dp.middleware.setup(PaymentFilterMiddleware(authorized_users))
