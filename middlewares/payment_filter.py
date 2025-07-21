68fc38c6bce3b719f2b1432df4f19658cf4551be
nathan <vinteo.ac@gmail.com>
2025-07-20 19:27:36 -0500

    mettre les liens sous formes de bouton

--- middlewares/payment_filter.py
+++ middlewares/payment_filter.py
@@ -2,6 +2,8 @@ from detect_links_whitelist import lien_non_autorise  # Pour filtrer les liens
 from aiogram import types
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.dispatcher.handler import CancelHandler
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import Bot
 
 ADMIN_ID = 7334072965  # Ton ID Telegram admin
 
@@ -14,16 +16,15 @@ BOUTONS_AUTORISES = [
 ]
 
 class PaymentFilterMiddleware(BaseMiddleware):
-    def __init__(self, authorized_users):
+    def __init__(self, authorized_users, bot: Bot):
         super(PaymentFilterMiddleware, self).__init__()
         self.authorized_users = authorized_users
+        self.bot = bot
 
     async def on_pre_process_message(self, message: types.Message, data: dict):
-
-        
         if message.content_type != types.ContentType.TEXT:
             return
-        
+
         # ➡️ SI c'est l'ADMIN, vérifier uniquement les liens
         if message.from_user.id == ADMIN_ID:
             if lien_non_autorise(message.text):
@@ -37,26 +38,26 @@ class PaymentFilterMiddleware(BaseMiddleware):
 
         if message.text and message.text.startswith("/start"):
             return  # Laisser passer /start
-        
+
         if message.text.strip() in BOUTONS_AUTORISES:
             return  # Laisser passer les boutons autorisés
-        
+
         if message.from_user.id not in self.authorized_users:
             try:
                 await message.delete()
             except Exception as e:
                 print(f"Erreur suppression message non autorisé : {e}")
-                await message.answer(
-    "🚫 Pour discuter librement avec moi, il faudra être un VIP !\n\n"
-    "👇 Clique ci-dessous pour débloquer ton accès immédiat :\n\n"
-    "Cela coûte 1€ en paiement unique ! 🎁 Je t'attends...🤭",
-    reply_markup=InlineKeyboardMarkup().add(
-        InlineKeyboardButton(
-            text="💎 Devenir VIP pour 1€",
-            url="https://buy.stripe.com/4gwg32fhF4K62fCdQR"
-        )
-    )
-)
+
+            await self.bot.send_message(
+                message.chat.id,
+                "🚫 Pour discuter librement avec moi, il faudra être un VIP !\n\n"
+                "👇 Clique ci-dessous pour débloquer ton accès immédiat :\n\n"
+                "Cela coûte 1€ en paiement unique ! 🎁 Je t'attends...🤭",
+                reply_markup=InlineKeyboardMarkup().add(
+                    InlineKeyboardButton(
+                        text="💎 Devenir VIP pour 1€",
+                        url="https://buy.stripe.com/4gwg32fhF4K62fCdQR"
+                    )
+                )
+            )
             raise CancelHandler()
-
-
