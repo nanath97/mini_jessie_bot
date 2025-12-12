bd242f036e02811a593848b304748d5bbeb2c82a
nathan <vinteo.ac@gmail.com>
2025-12-12 08:07:16 -0500

    test final

--- ai_state_store.py
+++ ai_state_store.py
@@ -2,15 +2,17 @@ import os
 import requests
 
 AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
-AIRTABLE_BASE_ID = os.getenv("AIRTABLE_BASE_ID")
-AI_STATE_TABLE = "AI_STATE"
+BASE_ID = os.getenv("BASE_ID")
 
-BASE_URL = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AI_STATE_TABLE}"
+AI_STATE_TABLE = os.getenv("AIRTABLE_TABLE_AI_STATE", "AI_STATE")
+AI_STATE_TABLE_URL = AI_STATE_TABLE.replace(" ", "%20")
+
+BASE_URL = f"https://api.airtable.com/v0/{BASE_ID}/{AI_STATE_TABLE_URL}"
+
 HEADERS = {
     "Authorization": f"Bearer {AIRTABLE_API_KEY}",
     "Content-Type": "application/json"
 }
-
 
 def get_state(telegram_id: int):
     formula = f"{{Telegram ID}}='{telegram_id}'"
